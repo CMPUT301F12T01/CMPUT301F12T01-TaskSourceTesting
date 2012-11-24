4f914fcc569ce878be8860a360659f31e106d2c1
Eddie Santos <easantos@ualberta.ca>
2012-11-24 14:52:08 -0700

    Moved StringUtils tests to the proper project.

--- TaskSourcePlainTests/test/ca/ualberta/cs/c301f12t01/test/StringUtilTest.java
+++ TaskSourcePlainTests/test/ca/ualberta/cs/c301f12t01/test/StringUtilTest.java
@@ -1,14 +1,17 @@
 package ca.ualberta.cs.c301f12t01.test;
 
+import static org.junit.Assert.*;
 import java.util.Arrays;
 
+import org.junit.Test;
+
 import ca.ualberta.cs.c301f12t01.util.StringUtils;
-import android.test.AndroidTestCase;
 
 /* TODO: write more tests and comments. */
 
-public class StringUtilTest extends AndroidTestCase {
+public class StringUtilTest {
 
+    @Test
     public void testOnStrings() {
         final String expectedResult = "String A, String B, String C"; 
         
